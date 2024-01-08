188a7bc26e92e83686bca3e732059d6f7b7a4ff9
RuthDahan <ruthftj@gmail.com>
2024-01-08 21:48:07 +0200

    Corrected content

--- src/About2.js
+++ src/About2.js
@@ -5,16 +5,16 @@ function About2() {
         <div   style={{fontSize:"27px"}}>
             <p><br/>
                 <h4>What is interesting about perfume?</h4>
-                <p>perfumes come from the island of Cyprus, nearly 4000 years ago, but the Egyptians were the ones to make perfume truly a part of their culture 3000 years ago.<br />
-                    The first actual factory dedicated to the production of perfumed oils dates back 4000 years on</p><br/>
+                <p>perfumes come from the island of Cyprus, nearly 4000 years ago, but the Egyptians were the ones to make perfume truly a part of their culture 3000 years ago.<br />The first actual factory dedicated to the production of perfumed oils dates back 4000 years on</p><br/>
+                
                 <h4>what do you call someone who loves perfume?</h4>
-                <p>
-                    In the World of PAIRFUM, a 'CognoScenti' is a person who 'Loves' perfume, a connoisseur who can distinguish between a good and a bad fragrance.</p><br/>
+                <p>In the World of PAIRFUM, a 'CognoScenti' is a person who 'Loves' perfume, a connoisseur who can distinguish between a good and a bad fragrance.</p><br/>
+                
                 <h4>which country is famous for perfume?</h4>
                 <p>France. France is renowned for its perfume industry and is home to some of the world's most famous perfumeries</p><br/>
+                
                 <h4>Why is perfume a luxury?</h4>
-                <p>The 
-                    Rare ingredients are the main thing that increases the cost of perfumes. Some contain petals of rare flowers or extracts of unusual roots, some bloom only one month a year</p><br/>
+                <p>The Rare ingredients are the main thing that increases the cost of perfumes. Some contain petals of rare flowers or extracts of unusual roots, some bloom only one month a year</p><br/>
             </p>
         </div>
     )
